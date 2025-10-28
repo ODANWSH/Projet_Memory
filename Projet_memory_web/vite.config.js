2bf1e33603180cdf2ef0ee9f3d03ce387bbbd3d4
ODANWSH <timeo.minarditm@gmail.com>
2025-10-28 15:50:30 +0100

    refactor: change to launch api and the app at the same time

--- Projet_memory_web/vite.config.js
+++ Projet_memory_web/vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  root: "./",
+  publicDir: "public",
+  server: {
+    port: 3000,
+    host: false, // A changer si besoin d'accès via réseau
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
+});
